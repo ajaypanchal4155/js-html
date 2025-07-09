f6409934f6688e33c87d594a68c7be39f4c7a33a
ajaypanchal4155 <ajaypanchal4534@gmail.com>
2025-07-09 11:13:21 +0000

    conversion

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -1,9 +1,9 @@
-let score ="hitesh"
+//let score ="hitesh"
 
 //console.log(typeof score);
 //console.log(typeof(score));
 
-let valueInnumber = number(score)
+//let valueInnumber = number(score)
 //console.log(typeof valueInnumber);
 //console.log(valueInnumber);
 
@@ -11,18 +11,23 @@ let valueInnumber = number(score)
 // "33abc" = NaN
 // true = 1; false = 0
 
-let IsloggedIn = "hitesh"
+let isloggedIn = "hitesh"
 
-let booleanIsloggedIn = boolean(IsloggedIn)
-console.log(booleanIsLoggedIn);
+let booleanIsloggedIn = Boolean(isloggedIn)
+//console.log(booleanIsloggedIn);
 
 // 1 = true; 0 = false
 // "" = false
 // "hitesh" = true
 
+let someNumber = 33
+let stringNumber = String(someNumber)
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
 let value = 3
 let negValue = -value
-//console.log(negValue);
+//console.log(negValue)
 
 //console.log(2+2);
 //console.log(2-2);
@@ -31,30 +36,24 @@ let negValue = -value
 //console.log(2/3);
 //console.log(2%3);
 
-let str1 ="hello"
-let str2 ="hitesh"
+let str1 = "hello"
+let str2 = "hitesh"
 
-let str3 =str1 + str2
-
-console.log(str3);
+let str3 = str1 + str2
+//console.log(str3);
 
 //console.log("1"+ 2);
 //console.log(1 + "2");
 //console.log("1" + 2 + 2);
-//console.log("1"+ 2 +"2");
+//console.log(1 + 2 + "2");
+//console.log((3+4) * 5 % 3);
 
-//console.log((3 + 4) * 5 % 3);
+//console.log(+true);
+//console.log(+"");
 
-console.log(+true);
-console.log(+"");
-
-let num1, num2, num3 
-
+let num1,num2,num3 
 num1 = num2 = num3 = 2 + 2
 
-let gameCounter = 100
-gameCounter++;
+let gameCounter = 100;
+++gameCounter;
 console.log(gameCounter);
-
-// link to study
-https//tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion

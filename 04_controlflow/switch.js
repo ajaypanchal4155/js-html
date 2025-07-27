4cb91f98319c53733707534d03833f5e4b32aa5c
ajaypanchal4155 <ajaypanchal4534@gmail.com>
2025-07-27 09:55:29 +0000

    switch

---
+++ 04_controlflow/switch.js
@@ -0,0 +1,27 @@
+//Switch(key){
+   // case value;
+
+   //break;
+
+   //default
+   //break;
+//}
+const month = "march"
+switch(month){
+    case "jan":
+        console.log("january");
+        break;
+        case "feb":
+            console.log("feb");
+            break;
+            case "Mar":
+            console.log("march");
+            break;
+            case "april":
+                console.log("april");
+                break;
+
+                default:
+                    console.log("default case match");
+                    break;
+}

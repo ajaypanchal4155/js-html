bb9f0ce10bb7ad46e946a9b2d7253f5895f791a3
ajaypanchal4155 <ajaypanchal4534@gmail.com>
2025-07-27 10:37:24 +0000

    truthy

---
+++ 04_controlflow/truthy.js
@@ -0,0 +1,43 @@
+const UserEmail = []
+//if (UserEmail){
+//console.log("got user email");
+//}else{
+   // console.log("Do not have user email");
+//}
+
+//falsy values
+
+//false, 0 , -0, bigint 0n, "", null,undefined,NaN
+
+//truthy values
+
+//"0",'false', "",[],{},function(){}
+
+//if (UserEmail.length === 0){
+//console.log("array is empty");
+
+//}
+//const emptyobj = {}
+
+//if(Object.keys(emptyobj).length === 0){
+   // console.log("object is empty");
+//}
+
+//nullish caolescing operator(??):null undefined
+
+//let val1;
+//val1  = 5??10
+//val1 = null ??10
+//val1 = undefined ?? 15
+//val1 = null ?? 10 ?? 20
+//console.log(val1);
+
+//terniary operator
+
+//condition ? true : false
+
+const iceTeaprice = 100
+iceTeaprice <= 80? console.log("less than 80"):
+console.log("more than 80");
+
+

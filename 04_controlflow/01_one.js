ceab9271c337d0146bba7189c2d4505a30f7b6c2
ajaypanchal4155 <ajaypanchal4534@gmail.com>
2025-07-27 09:36:55 +0000

    if - else

---
+++ 04_controlflow/01_one.js
@@ -0,0 +1,46 @@
+//if
+//const isUserloggedIn = true
+//const temperature = 41
+//if (temperature < 50){
+   // console.log("less than 50");
+//}
+//else
+//{
+   // console.log("temperature is greater than 50");
+//}
+//< , >, <=,>=,==,!=,===
+
+//const score = 200
+//if (score > 100){
+    //let power = "fly"
+    //console.log('user power:${power}');
+//}
+//console.log('user power:${power}');
+
+const balance = 1000
+
+ //if(balance > 500)console.log("test"),console.log("test2");
+
+ //if (balance<500){
+   // console.log("less than");
+ //}else if(balance < 750){
+    //console.log("less than 750");
+ //}
+    //else if(balance < 900){
+    //    console.log("less than 750");
+    //}
+ //else{
+   // console.log("less than 1200");
+ //}
+
+ const UserLoggedIn = true
+ const debitcard = true
+ const loggedInFromGoogle = false
+ const loggedinfromEmail = true
+ if(UserLoggedIn && debitcard){
+    console.log("allow to buy course");
+ }
+if (loggedInFromGoogle || loggedinfromEmail)
+{
+    console.log("User logged in");
+}

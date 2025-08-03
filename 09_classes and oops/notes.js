79613b3857ccede47bcfd1d87abd274f2fe69677
ajaypanchal4155 <ajaypanchal4534@gmail.com>
2025-08-03 07:21:31 +0000

    notes

---
+++ 09_classes and oops/notes.js
@@ -0,0 +1,17 @@
+//#javascript and classes
+//#oop
+//#object
+//-Collection of Properties and methods
+//-toLowerCase
+//##why use oop
+//## parts of oop
+//object literal
+//- Constructor function
+//- Prototypes
+//- classes
+//-Instances(new,this)
+//4 pillars
+//Abstraction
+//Encapsulation
+//Inheritance
+//polymorphism

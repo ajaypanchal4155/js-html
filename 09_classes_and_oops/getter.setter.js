8cad3e13cdd1c0b068bd23eddb8d8eb01af3ff13
ajaypanchal4155 <ajaypanchal4534@gmail.com>
2025-08-04 08:06:40 +0000

    get set

---
+++ 09_classes_and_oops/getter.setter.js
@@ -0,0 +1,20 @@
+class User{
+    constructor(email,password){
+        this.email = email;
+        this.password = password
+    }
+    get email(){
+        return this._email.toUpperCase()
+    }
+    set email(value){
+        this._email= value
+    }
+    get password(){
+        return`${this._password.toUpperCase()}`;
+    }
+    set password(value){
+        return this._password = value
+    }
+}
+const hitesh = new User ("h@hitesh.ai","123")
+console.log(hitesh.password);
